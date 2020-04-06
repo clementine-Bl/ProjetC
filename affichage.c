ed6d44de13301b88c13b856ab77d5d5e906afeca
PROPRIETAIRE <clem.060799@gmail.com>
2020-04-06 17:01:42 +0200

    essaie affichage modification /

--- affichage.c
+++ affichage.c
@@ -1,16 +1,19 @@
 #include "affichage.h"
 
+
+
 void affichage(int x,int y){
     if (access(".verrouData",F_OK)!=-1){
-
+        printf("Attention l'interface lit les données");
     }else{
-        FILE* pf=fopen("C:\\Users\PROPRIETAIRE\Desktop\Interface\Data.txt","w+");
+        FILE* pf=fopen("Data.txt","w+");
         if(pf==NULL){
             printf("dans fichiers.c : erreur ouverture fichier ");
             exit(EXIT_FAILURE);
         }else{
+
             fprintf(pf,"%d\n%d",x,y);
-            fclose(pf);
+            remove(".verrouData");
         }
 
     }
--- main.c
+++ main.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include "affichage.h"
-#include "fichiers.h"
 
 int main() {
-    int etat=0;
-    absorp myAbsorp;
     printf("coucou\n");
-    affichage(100,5);
-    FILE* myFile = initFichier("record1.dat");
-    myAbsorp = lireFichier(myFile,&etat);
+    affichage(60,90);
     return 0;
 }
