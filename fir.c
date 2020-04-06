959286bf1d11804d3e62a88db81895cc185fb14f
chloe-kammerlocher <chloe.kammerlocher@isen-ouest.yncrea.fr>
2020-04-06 12:05:37 +0200

    insertion tous les fichiers

--- fir.c
+++ fir.c
@@ -1,5 +1,8 @@
-//
-// Created by PROPRIETAIRE on 06/04/2020.
-//
-
 #include "fir.h"
+
+absorp firTest(char* filename){
+	absorp	myAbsorp;
+	
+	return myAbsorp;
+
+}
--- fir.h
+++ fir.h
@@ -1,8 +1,3 @@
-//
-// Created by PROPRIETAIRE on 06/04/2020.
-//
+#include "define.h"
 
-#ifndef PROJETC_FIR_H
-#define PROJETC_FIR_H
-
-#endif //PROJETC_FIR_H
+absorp firTest(char* filename);
