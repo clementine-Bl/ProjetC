48131107d32a008f5f2dbd5ce3047b44a165078a
PROPRIETAIRE <clem.060799@gmail.com>
2020-04-06 14:27:43 +0200

    essaie affichage modification /

--- affichage.c
+++ affichage.c
@@ -4,7 +4,7 @@ void affichage(int x,int y){
     if (access(".verrouData",F_OK)!=-1){
 
     }else{
-        FILE* pf=fopen("C:\\Users\PROPRIETAIRE\Desktop\Interface/Data.txt","w+");
+        FILE* pf=fopen("C:\\Users\PROPRIETAIRE\Desktop\Interface\Data.txt","w+");
         if(pf==NULL){
             printf("dans fichiers.c : erreur ouverture fichier ");
             exit(EXIT_FAILURE);
