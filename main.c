7608ad4fc0925d84f05c33040ccad3a7c750e5d0
chloe-kammerlocher <chloe.kammerlocher@isen-ouest.yncrea.fr>
2020-04-07 17:19:55 +0200

    main

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include "affichage.h"
+#include "iir.h"
+#include "fir.h"
+#include "mesure.h"
+#include "integration.h"
 
 int main() {
-    printf("coucou\n");
-    affichage(60,100);
+    /*absorp valeur =iirTest("record1_fir.dat");
+    printf("%f,%f,%f,%f\n", valeur.acr,valeur.dcr,valeur.acir,valeur.dcir);
+    absorp valeur2 =firTest("record1.dat");
+    printf("%f,%f,%f,%f\n", valeur2.acr,valeur2.dcr,valeur2.acir,valeur2.dcir);
+    oxy mesure1=mesureTest("record1_iir.dat");
+    printf("%d,%d\n", mesure1.spo2, mesure1.pouls);
+    affichage(mesure1);*/
+    integrationTest("record1.dat");
     return 0;
 }
