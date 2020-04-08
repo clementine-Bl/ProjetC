253718672c076e54e38b35460ee911031a9e0feb
PROPRIETAIRE <clem.060799@gmail.com>
2020-04-08 11:01:24 +0200

    Merge branch 'master' of C:\Users\PROPRIETAIRE\Desktop\ProjetC with conflicts.

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 #include "integration.h"
 
 int main() {
-    //absorp valeur =iirTest("record1_fir.dat");
-    //printf("%f,%f,%f,%f\n", valeur.acr,valeur.dcr,valeur.acir,valeur.dcir);
-    //absorp valeur2 =firTest("record1.dat");
-    //printf("%f,%f,%f,%f\n", valeur2.acr,valeur2.dcr,valeur2.acir,valeur2.dcir);
-    //oxy mesure1=mesureTest("record1_iir.dat");
-    //printf("%d,%d\n", mesure1.spo2, mesure1.pouls);
-    //affichage(mesure1);
+    /*absorp valeur =iirTest("record1_fir.dat");
+    printf("%f,%f,%f,%f\n", valeur.acr,valeur.dcr,valeur.acir,valeur.dcir);
+    absorp valeur2 =firTest("record1.dat");
+    printf("%f,%f,%f,%f\n", valeur2.acr,valeur2.dcr,valeur2.acir,valeur2.dcir);
+    oxy mesure1=mesureTest("record1_iir.dat");
+    printf("%d,%d\n", mesure1.spo2, mesure1.pouls);
+    affichage(mesure1);*/
     integrationTest("record1.dat");
     return 0;
 }
