2e912455ec752059c661e5a0ab3f0f6d0ce926bb
PROPRIETAIRE <clem.060799@gmail.com>
2020-04-08 18:06:45 +0200

    mesure

--- mesure.c
+++ mesure.c
@@ -67,71 +67,70 @@ oxy MESURE(absorp myAbsorp, float* tableau,oxy myOxy){
             }
         }
     }else{
-            if(tableau[4]==0){
+        if(tableau[4]==0){
+            if (tableau[7]==1) {
+                tableau[7] = 0;
+            }else {
+                tableau[7] = 1;
+            }
+            tableau[0] = myAbsorp.acr; // initialisation du min ac_r
+            tableau[1]= myAbsorp.acr; // initialisation du max ac_r
+            tableau[2]= myAbsorp.acir; // initialisation du min ac_ir
+            tableau[3]= myAbsorp.acir; // initialisation du max ac_ir
+            tableau[4] = tableau[4] + 1; // on incrémente le compteur de 1 car on a étudié une valeur de ac
+            tableau[5]=0;
+        } else {
+            if (tableau[5] != 1) { // On fait tant qu'une demi periode n'est pas faite
+                if ((tableau[7] ==0 && myAbsorp.acr<=0)||(tableau[7] == 1 && myAbsorp.acr >= 0)){
+                    /* Si le debut commençait de façon croissante et que notre nouvelle valeur est plus petite que notre valeur de départ alors
+                    une demi periode est passé*/
+                    /* Ou bien si le debut commençait de façon décroissante et que notre nouvelle valeur est plus grande que notre valeur de départ alors
+                    une demi periode est passé*/
+                    tableau[5] = 1;
                     if (tableau[7]==1) {
                         tableau[7] = 0;
                     }else {
                         tableau[7] = 1;
                     }
-                    tableau[0] = myAbsorp.acr; // initialisation du min ac_r
-                    tableau[1]= myAbsorp.acr; // initialisation du max ac_r
-                    tableau[2]= myAbsorp.acir; // initialisation du min ac_ir
-                    tableau[3]= myAbsorp.acir; // initialisation du max ac_ir
-                    tableau[4] = tableau[4] + 1; // on incrémente le compteur de 1 car on a étudié une valeur de ac
-                    tableau[5]=0;
+                }
+                mise_a_jour(&tableau[4], myAbsorp.acr, &tableau[0], &tableau[1], myAbsorp.acir, &tableau[2],&tableau[3]);
             } else {
-                    if (tableau[5] != 1) { // On fait tant qu'une demi periode n'est pas faite
-                        if ((tableau[7] ==0 && myAbsorp.acr<=0)||(tableau[7] == 1 && myAbsorp.acr >= 0)){
-                            /* Si le debut commençait de façon croissante et que notre nouvelle valeur est plus petite que notre valeur de départ alors
-                             une demi periode est passé*/
-                            /* Ou bien si le debut commençait de façon décroissante et que notre nouvelle valeur est plus grande que notre valeur de départ alors
-                             une demi periode est passé*/
-                            tableau[5] = 1;
-                            if (tableau[7]==1) {
-                                tableau[7] = 0;
-                            }else {
-                                tableau[7] = 1;
-                            }
-                        }
-                        mise_a_jour(&tableau[4], myAbsorp.acr, &tableau[0], &tableau[1], myAbsorp.acir, &tableau[2],&tableau[3]);
+                if ((tableau[7] ==0 && myAbsorp.acr<=0)||(tableau[7] == 1 && myAbsorp.acr >= 0)) {
+                    /*on sait que une demi periode est déjà passée
+                    Si le debut commençait de façon croissante et que notre nouvelle valeur est plus grande que notre valeur de départ alors
+                    une periode est passée en tout */
+                    /* Ou bien si le debut commençait de façon décroissante et que notre nouvelle valeur est plus petite que notre valeur de départ alors
+                    une periode est passée en tout */
+
+                    ratio = ((tableau[1] - tableau[0]) / myAbsorp.dcr) / ((tableau[3] - tableau[2]) / myAbsorp.dcir);
+                    /* Calcul du ratio : Tableau[1]-Tableau[0] : max-min amplitude crête à crête de ac_r
+                                        Tableau[3]-Tableau[2] : max-min amplitude crête à crête de ac_ir*/
+
+                    if (ratio <=1) {   //l'équation entre le ration et spo2 est différente si on est inférieur ou supérieur à 1
+                        myOxy.spo2 = (-25) * ratio + 110;   //équations trouvé à l'aide du graphique
                     } else {
-                        if ((tableau[7] ==0 && myAbsorp.acr<=0)||(tableau[7] == 1 && myAbsorp.acr >= 0)) {
-                            /*on sait que une demi periode est déjà passée
-                             Si le debut commençait de façon croissante et que notre nouvelle valeur est plus grande que notre valeur de départ alors
-                             une periode est passée en tout */
-                            /* Ou bien si le debut commençait de façon décroissante et que notre nouvelle valeur est plus petite que notre valeur de départ alors
-                             une periode est passée en tout */
-
-                            ratio = ((tableau[1] - tableau[0]) / myAbsorp.dcr) / ((tableau[3] - tableau[2]) / myAbsorp.dcir);
-                            /* Calcul du ratio : Tableau[1]-Tableau[0] : max-min amplitude crête à crête de ac_r
-                                                 Tableau[3]-Tableau[2] : max-min amplitude crête à crête de ac_ir*/
-
-                            if (ratio <=1) {   //l'équation entre le ration et spo2 est différente si on est inférieur ou supérieur à 1
-                                myOxy.spo2 = (-25) * ratio + 110;   //équations trouvé à l'aide du graphique
-                            } else {
-                                myOxy.spo2 = (-35.7) * ratio + 121.38;  //équations trouvé à l'aide du graphique
-                            }
-                            for (i = 8; i < 11; i++) {
-                                if (tableau[i] != 0) {
-                                    tableau[11] += tableau[i];
-                                    compteur_periode += 1;
-                                }
-                            }
-                            myOxy.pouls = ((30000 / tableau[4]) + tableau[11]) /compteur_periode; //formule pour calculer la frequence en BPM à partir du nombre de valeur prise pendant une periode
-                            for (i = 10; i > 8; i--) {
-                                tableau[i] = tableau[i - 1];
-                            }
-                            tableau[8] = 30000 / tableau[4];
-                            tableau[11] = 0;
-                            tableau[4] = 0; // on remet le compteur de valeur à 0 car on a fini une periode
-
-                        } else {
-                            mise_a_jour(&tableau[4], myAbsorp.acr, &tableau[0], &tableau[1], myAbsorp.acir, &tableau[2],&tableau[3]);
+                        myOxy.spo2 = (-35.7) * ratio + 121.38;  //équations trouvé à l'aide du graphique
+                    }
+                    for (i = 8; i < 11; i++) {
+                        if (tableau[i] != 0) {
+                            tableau[11] += tableau[i];
+                            compteur_periode += 1;
                         }
                     }
+                    myOxy.pouls = ((30000 / tableau[4]) + tableau[11]) /compteur_periode; //formule pour calculer la frequence en BPM à partir du nombre de valeur prise pendant une periode
+                    for (i = 10; i > 8; i--) {
+                        tableau[i] = tableau[i - 1];
+                    }
+                    tableau[8] = 30000 / tableau[4];
+                    tableau[11] = 0;
+                    tableau[4] = 0; // on remet le compteur de valeur à 0 car on a fini une periode
+
+                } else {
+                    mise_a_jour(&tableau[4], myAbsorp.acr, &tableau[0], &tableau[1], myAbsorp.acir, &tableau[2],&tableau[3]);
                 }
             }
         }
+    }
     return myOxy;
 }
 
