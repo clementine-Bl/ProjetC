91a17847a57e8aa5beffb91fa2e6662417773392
PROPRIETAIRE <clem.060799@gmail.com>
2020-04-08 12:07:08 +0200

    iir commentaire

--- iir.c
+++ iir.c
@@ -17,18 +17,27 @@ absorp iirTest(char* filename){
 }
 
 absorp IIR (absorp my, float ** tableau){
-    float y1r = my.acr-tableau[0][0]+0.992*tableau[0][1];
-    tableau[0][0]=my.acr;
-    my.acr=y1r;
-    float y1ir = my.acir-tableau[1][0]+0.992*tableau[1][1];
-    tableau[1][0]=my.acir;
-    my.acir=y1ir;
+    float y1r = my.acr-tableau[0][0]+0.992*tableau[0][1]; // calcul de y[n] de ac_r
+    tableau[0][0]=my.acr; // remplace x[n-1] par x[n] de ac_r
+    my.acr=y1r; // on modifie le acr de absorp par notre nouvelle valeur
+    float y1ir = my.acir-tableau[1][0]+0.992*tableau[1][1]; // calcul de y[n] de ac_ir
+    tableau[1][0]=my.acir; // remplace x[n-1] par x[n] de ac_ir
+    my.acir=y1ir; // on modifie le acir de absorp par notre nouvelle valeur
     tableau[0][1]=y1r;
-    tableau[1][1]=y1ir;
+    tableau[1][1]=y1ir; // Sur les deux dernière ligne on remplace nos y[n-1] de ac_r et ac_ir
     return my;
 }
 
 float** create_tableau_IIR(){
+    /*Detail tableau:
+     Tableau[0] ligne de ac_r
+        - Tableau[0][0] correspond a : x[n-1]
+        - Tableau[0][1] correspond a : y[n-1]
+
+     Tableau[1] ligne de ac_ir
+        - Tableau[1][0] correspond a : x[n-1]
+        - Tableau[1][1] correspond a : y[n-1]
+     Fin détail tableau */
     float** tableau;
     int i;
     tableau=malloc(2* sizeof(float*));
@@ -36,7 +45,7 @@ float** create_tableau_IIR(){
         tableau[0]=malloc(2* sizeof(float));
         tableau[1]=malloc(2* sizeof(float));
         if (tableau[0] != NULL && tableau[1] != NULL) {
-            for (i=0;i<2;i++) {
+            for (i=0;i<2;i++) { // on initialise tout à 0 car au début nous n'avons pas les valeurs
                 tableau[0][i] = 0;
                 tableau[1][i] = 0;
             }
