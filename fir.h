18ac22e771f37e997a3918098fbc134d708720cd
PROPRIETAIRE <clem.060799@gmail.com>
2020-04-08 11:25:57 +0200

    tableau global

--- fir.h
+++ fir.h
@@ -4,3 +4,4 @@ absorp firTest(char* filename);
 absorp FIR(absorp valueAbsorp,int *cpt, float** tableau);
 float** create_tableau_FIR();
 void supprime_tableau_FIR(float** tableau);
+float FIR_TAPS[51]; // tableau des coefficients de h
