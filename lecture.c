569d9d5c0b14fa2b5977e6b95c4297d99e1c92a3
chloe-kammerlocher <chloe.kammerlocher@isen-ouest.yncrea.fr>
2020-04-09 10:39:01 +0200

    lecture

--- lecture.c
+++ lecture.c
@@ -2,28 +2,19 @@
 #include "fichiers.h"
 
 absorp lecture(FILE* file_pf, int* file_state){
-    int octet, valeur;
-    int i=1;
+    int octet;
+    float valeur=0;
+    int i=1, cpt=1;
     absorp myAbsorp;
-    while(i<22) {
-        if (i<5){
-            octet = fgetc(file_pf);
-            octet = octet - 48;
-            valeur =
-        }
-        if (5<i<10){
-            octet = fgetc(file_pf);
-            octet = octet - 48;
-        }
-        if (10<i<15){
-            octet = fgetc(file_pf);
-            octet = octet - 48;
-        }
-        if (15<i<20){
-            octet = fgetc(file_pf);
-            octet = octet - 48;
-        }
+    while(cpt<5) {
+        octet = fgetc(file_pf);
+        octet = octet - 48;
+        additoner(valeur,i,octet);
         i++;
+        if (i==5){
+            modifier(myAbsorp,cpt,valeur);
+            cpt ++;
+        }
     }
     if(feof(file_pf)) {
         *file_state = EOF;
@@ -32,3 +23,34 @@ absorp lecture(FILE* file_pf, int* file_state){
 
 }
 
+float additoner (float nombre, int compteur, int chiffre){
+    if(compteur%4==1){
+        nombre += 1000*chiffre;
+    }
+    if(compteur%4==2){
+        nombre += 100*chiffre;
+    }
+    if(compteur%4==3){
+        nombre += 10*chiffre;
+    }
+    if(compteur%4==0){
+        nombre += 1*chiffre;
+    }
+    return nombre;
+}
+
+absorp modifier  (absorp my, int compteur, float valeur){
+    if(compteur%4==1){
+        my.acr=valeur;
+    }
+    if(compteur%4==2){
+        my.dcr=valeur;
+    }
+    if(compteur%4==3){
+        my.acir=valeur;
+    }
+    if(compteur%4==0){
+        my.dcir=valeur;
+    }
+    return my;
+}
--- lecture.h
+++ lecture.h
@@ -2,3 +2,5 @@
 
 
 absorp lecture(FILE* file_pf, int* file_state);
+float additoner (float nombre, int compteur, int chiffre);
+absorp modifier  (absorp my, int compteur, float valeur);
