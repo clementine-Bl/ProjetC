38b67fbf50a527f01d117de8faa5844256f70ac2
PROPRIETAIRE <clem.060799@gmail.com>
2020-04-09 10:03:48 +0200

    mesure

--- lecture.c
+++ lecture.c
@@ -1,8 +1,14 @@
 #include "lecture.h"
-
+#include "fichiers.h"
 
 absorp lecture(FILE* file_pf, int* file_state){
-
+    int octet;
+    int i =0;
+    while(i<21) {
+        octet = fgetc(file_pf);
+        printf("%d\n", octet);
+        i++;
+    }
 	absorp myAbsorp;
 	*file_state=EOF;
 	
--- main.c
+++ main.c
@@ -4,9 +4,14 @@
 #include "fir.h"
 #include "mesure.h"
 #include "integration.h"
+#include "lecture.h"
+#include "fichiers.h"
 
 int main() {
-    printf("clementine beug");
+    int file_state;
+    absorp myAbsorp;
+    FILE* file_pf=initFichier("record1_bin.dat");
+    myAbsorp=lecture(file_pf,&file_state);
     /*absorp valeur =iirTest("record1_fir.dat");
     printf("%f,%f,%f,%f\n", valeur.acr,valeur.dcr,valeur.acir,valeur.dcir);
     absorp valeur2 =firTest("record1.dat");
@@ -14,6 +19,6 @@ int main() {
     oxy mesure1=mesureTest("record1_iir.dat");
     printf("%d,%d\n", mesure1.spo2, mesure1.pouls);
     affichage(mesure1);*/
-    integrationTest("record1.dat");
+    //integrationTest("record1.dat");
     return 0;
 }
