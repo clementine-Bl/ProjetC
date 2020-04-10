c2d6bf7b50508870f1ea3e659c4ae01e5d2694c6
PROPRIETAIRE <clem.060799@gmail.com>
2020-04-10 09:29:24 +0200

    mesure

--- mesure.h
+++ mesure.h
@@ -4,10 +4,10 @@
 
 oxy mesureTest(char* filename);
 float* create_tableau_mesure();
-void min_max(float value,float* min, float *max);
+void min_max(float value,float* min, float* max);
 oxy MESURE(absorp myAbsorp, float* tableau, oxy myOxy);
 void supprime_tableau_mesure(float* tableau);
-void mise_a_jour(float value1, float value2,float* tableau);
+void mise_a_jour(float value1,float value2, float* tableau);
 void changement_de_signe(float* tableau7);
 
 #endif
